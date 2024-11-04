2371e49e4ee69f4707b56369fc9d07b4dd16945f
Pratham <pratham.sethi@mitratech.com>
2024-11-04 13:49:51 +0530

    tabling the rows

--- main.js
+++ main.js
@@ -10,14 +10,29 @@ if (isMobile) {
   document.getElementById("header").innerText = "Nope, not mobile so seeing table";
 }
 
-let htmlTable = data.map(function(el){
-    
-   let parentDiv = `<div class='level-1'>${childrenDiv(el)}</div>`
-      return parentDiv;
+let htmlTable = data.map(function(el, index){
+    let headerDiv = '';
+   if(index==1 && isMobile == false){
+    headerDiv = `<div class='level-1 header'>${childrenDiv(el,true)}</div>`;
+    $('#table').prepend(headerDiv);
+   }
+   let parentDiv = `<div class='level-1'>${childrenDiv(el, false)}</div>`;
+   
+     return parentDiv;
 });
 
-function childrenDiv(el){
+function childrenDiv(el, header){
+
     let tempDiv = '';
+    let headerChildDiv = '';
+
+    if(header == true){
+        for (const [key] of Object.entries(el)) {
+            headerChildDiv +=`<div class="level-2">${key}</div>`;
+         }
+         
+         return headerChildDiv
+    }
     for (const [key, value] of Object.entries(el)) {
         tempDiv +=`<div class="level-2"><span class="label">${key}: </span>${value}</div>`;
      }
