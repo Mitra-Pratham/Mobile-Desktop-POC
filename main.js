80a3d9d2d8db05a21332cbf989e4a7d04321b7fd
Pratham <pratham.sethi@mitratech.com>
2024-11-05 23:04:22 +0530

    added content to the detail view and improved CSS

--- main.js
+++ main.js
@@ -4,58 +4,66 @@ const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 const rootMedium = document.querySelector(':root');
 if (isMobile) {
     document.getElementById("header").innerText = "Mobile; Loads with a Card view";
-    rootMedium.style.setProperty( '--template-column','repeat(1,1fr)');
-    rootMedium.style.setProperty( '--label-show','block');
-    rootMedium.style.setProperty( '--isMobile','true');
+    rootMedium.style.setProperty('--template-column', 'repeat(1,1fr)');
+    rootMedium.style.setProperty('--label-show', 'block');
+    rootMedium.style.setProperty('--isMobile', 'true');
 } else {
-  document.getElementById("header").innerText = "Desktop; Loads with a Table view";
+    document.getElementById("header").innerText = "Desktop; Loads with a Table view";
 }
 
-let htmlTable = data.map(function(el, index){
+let htmlTable = data.map(function (el, index) {
     let headerDiv = '';
-   if(index==1 && isMobile == false){
-    headerDiv = `<div class='header'>${childrenDiv(el,true)}</div>`;
-    $('#table').prepend(headerDiv);
-   }
-   let parentDiv = `<div class='row-card'>${childrenDiv(el, false)}</div>`;
-   
-     return parentDiv;
+    if (index == 1 && isMobile == false) {
+        headerDiv = `<div class='header'>${childrenDiv(el, true)}</div>`;
+        $('#table').prepend(headerDiv);
+    }
+    let parentDiv = `<div class='row-card'>${childrenDiv(el, false)}</div>`;
+
+    return parentDiv;
 });
 
-function childrenDiv(el, header){
+function childrenDiv(el, header) {
 
     let tempDiv = '';
     let headerChildDiv = '';
 
-    if(header == true){
+    if (header == true) {
         for (const [key] of Object.entries(el)) {
-            headerChildDiv +=`<div class="row-card-details">${key}</div>`;
-         }
-         return headerChildDiv
+            if (key != 'Description') {
+                headerChildDiv += `<div class="row-card-details">${key}</div>`;
+            }
+        }
+        return headerChildDiv
     }
 
     for (const [key, value] of Object.entries(el)) {
-        if(key === 'Name'){
-            tempDiv +=`<div class="row-card-details"><span class="label">${key}: </span><a href="#" class="name-link">${value}</a></div>`;
+        if (key === 'Name') {
+            tempDiv += `<div class="row-card-details"><span class="label">${key}: </span><a href="javascript:void(0)" class="name-link">${value}</a></div>`;
         }
-        else{
-            tempDiv +=`<div class="row-card-details"><span class="label">${key}: </span>${value}</div>`;
+        else if (key != 'Description') {
+            tempDiv += `<div class="row-card-details"><span class="label">${key}: </span>${value}</div>`;
         }
-     }
+    }
     return tempDiv;
 }
 
 $('#table').append(htmlTable);
 
 
-function openPanel(name){
-        $('#sidepanel').show();
-}
-
-$('.name-link').on('click', function(){
+$('.name-link').on('click', function (e) {
     $('#sidepanel').show();
+    let tempObj = data.find(el => {
+        return el.Name === $(this).text();
+    });
+    let tempDetails = '';
+    $('#sidepanel h3').text(tempObj.Name);
+    for (const [key, value] of Object.entries(tempObj)) {
+        tempDetails += `<div class="row-card-details"><span class="label">${key}: </span>${value}</div>`;
+    }
+    $('#panel-details').empty();
+    $('#panel-details').append(tempDetails);
 })
 
-$('#close-panel').on('click', function(){
+$('#close-panel').on('click', function () {
     $('#sidepanel').hide();
 })
